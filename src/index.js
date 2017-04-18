1c6c5c79f274fa73f29f5cbc66428c1ad6af953b
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-18 15:51:24 -0700

    tried to add react, got error

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-'use strict';
 //Create new component. This compment will produce HTML
 const App = function(){
   return <div>Hi!</div>; //jsx looks like html but is js
 };
-
+//jsx can not be interpreted by browser(webpack/babel compiles it into vanilla js)
+//write jsx to produce HTML that gets inserted when rendered
 
 //put into DOM components generated HTML
+React.render(App); //React not defined error in browser
