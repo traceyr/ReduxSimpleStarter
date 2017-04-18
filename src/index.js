3d4a38163d2b6056e22eed52af29e48a1b561c04
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-18 15:45:03 -0700

    added basic component, but did not attach it to dom yet

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 'use strict';
+//Create new component. This compment will produce HTML
+const App = function(){
+  return <div>Hi!</div>; //jsx looks like html but is js
+};
+
+
+//put into DOM components generated HTML
