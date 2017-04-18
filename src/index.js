59b57ccd27a8437502fd13fee922e321f80ba5cc
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-18 16:04:31 -0700

    working react component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
+//Gets react from installed modules
+import React from 'react'; //create and manage components
+import ReactDOM from 'react-dom'; //interacts with DOM
+
 //Create new component. This compment will produce HTML
-const App = function(){
+const App = () => {
   return <div>Hi!</div>; //jsx looks like html but is js
 };
 //jsx can not be interpreted by browser(webpack/babel compiles it into vanilla js)
 //write jsx to produce HTML that gets inserted when rendered
 
 //put into DOM components generated HTML
-React.render(App); //React not defined error in browser
+ReactDOM.render(<App />, document.querySelector('.container')); //inside tag creates instance of app, instead of a component class
+//second argument references where to put it on the DOM
