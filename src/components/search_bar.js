3120c025760bde2d42d8bbd35614cd1a5e74efab
Tracey Radcliffe <radcliffe.tracey@gmail.com>
2017-04-18 17:23:15 -0700

    added search bar component and exported it

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,1 +1,8 @@
+//creates search bar that updates video list
+import React from 'react';
 
+const SearchBar = () => {
+  return <input />;
+}
+
+export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@
 import React from 'react'; //create and manage components
 import ReactDOM from 'react-dom'; //interacts with DOM
 
+import SearchBar from './components/search_bar'; //gets component from search_bar.js that was exported
+
 const API_KEY = 'AIzaSyCl7wsYNF0wB2c5bfp2g4pf75l8z6yvdWo';
 //Create new component. This compment will produce HTML
 const App = () => {
-  return <div>Hi!</div>; //jsx looks like html but is js
-};
+  return (
+    <div>
+      <SearchBar />
+      </div> //jsx looks like html but is js
+    );
+}
 //jsx can not be interpreted by browser(webpack/babel compiles it into vanilla js)
 //write jsx to produce HTML that gets inserted when rendered
 
